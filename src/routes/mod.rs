6781ab5c7bb267003c36fde6da1c3af1668ef2b2
John Mark Banisilon <zionsploit@gmail.com>
2025-02-16 23:38:41 +0800

    feat(Development): :sparkles: Add API Routes for Projects and Project Details

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -3,3 +3,4 @@ pub mod account_route;
 pub mod project_interface_route;
 pub mod project_takers_route;
 pub mod contractors_route;
+pub mod project_route;
---
+++ src/routes/project_route.rs
@@ -0,0 +1,12 @@
+use axum::{routing::{get, post}, Router};
+
+use crate::services::project_service::{add_project, add_project_details, delete_project_by_id, get_all_projects, update_project_by_id};
+
+pub fn project_route () -> Router {
+    Router::new()
+        .route("/project-details-add", post(add_project_details))
+        .route("/add", post(add_project))
+        .route("/update-by-id", post(update_project_by_id))
+        .route("/delete-by-id", post(delete_project_by_id))
+        .route("/get-all", get(get_all_projects))
+}
