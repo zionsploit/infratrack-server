72a0595f4bbbbb03d1d0d20c6d6e117625779165
John Mark Banisilon <zionsploit@gmail.com>
2025-02-17 18:26:37 +0800

    refactor(Development): :recycle: add missing references for account_information

--- migrations/20250217095252_account_information.sql
+++ migrations/20250217095252_account_information.sql
@@ -9,4 +9,19 @@ CREATE TABLE IF NOT EXISTS account_information (
     position VARCHAR(255),
     created_at timestamp DEFAULT CURRENT_TIMESTAMP,
     updated_at timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+-- ALTER TABLE ADD MISSING COLUMN REFERENCES
+
+ALTER TABLE account_information ADD COLUMN (
+
+    account_cred_id VARCHAR(255) NOT NULL,
+    INDEX account_cred_id_ind (account_cred_id),
+    FOREIGN KEY (account_cred_id) REFERENCES account_credentials(id) ON DELETE CASCADE,
+
+    account_assets_id Int NOT NULL,
+    INDEX account_assets_id_ind (account_assets_id),
+    FOREIGN KEY (account_assets_id) REFERENCES account_assets(id) ON DELETE CASCADE,
+
+    UNIQUE(account_cred_id, account_assets_id)
 )
