05a48dd0330c1d935326cdcc85620f3c3503ffe8
John Mark Banisilon <zionsploit@gmail.com>
2025-02-20 13:48:35 +0800

    refactor(Development): :bug: translate into generate the filename and replace hardcoded url into .env from

--- src/services/storage_service.rs
+++ src/services/storage_service.rs
@@ -5,6 +5,7 @@ use std::sync::Arc;
 use axum::{extract::Multipart, response::IntoResponse, Extension};
 use http::StatusCode;
 use infer::MatcherType;
+use uuid::Uuid;
 
 use crate::{enums::response_enum::ResponseErrorMessage, structs::{account_assets::AccountAssets, pool_conn_struct::PoolConnectionState}, utils::storages::Storage};
 
@@ -16,7 +17,6 @@ pub async fn storage (
 
 
     if let Some(field) = multipart.next_field().await.unwrap() {
-        let get_file_name = field.name().unwrap().to_owned();
         let get_file_bytes = match field.bytes().await {
             Ok(result) => result.to_owned(),
             Err(_) => {
@@ -30,13 +30,16 @@ pub async fn storage (
             if mime_type.ne(&MatcherType::Image) {
                 return (StatusCode::BAD_REQUEST, format!("{:?}", ResponseErrorMessage::InvalidRequest));
             }
+            let host_server = dotenvy::var("HOST_SERVER").expect("Missing Host Server ENV");
+
+            let file_ext = Storage::get_file_ext(&get_file_bytes);
 
             let file_information = AccountAssets {
-                file_ext: Storage::get_file_ext(&get_file_bytes),
+                file_ext: file_ext.to_owned(),
                 file_hash: Storage::hash_file(&get_file_bytes),
-                file_name: get_file_name.to_string(),
+                file_name: format!("{}.{}", Uuid::new_v4().to_string().replace("-", ""), file_ext),
                 file_size: (&get_file_bytes.len().to_owned()).to_string(),
-                file_url: "http://localhost:something".to_owned()
+                file_url: format!("{}/storage/image", host_server)
             };
 
             match sqlx::query("INSERT INTO account_assets (file_name, file_ext, file_url, file_size, file_hash) VALUES (?, ?, ?, ?, ?)")
