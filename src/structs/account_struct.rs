5ea9760151f074177d37432199a3e4c889530fc9
John Mark Banisilon <zionsploit@gmail.com>
2025-02-22 17:30:04 +0800

    refactor(Development): :coffin: Remove unused structs
    this is the structs use in /register (Deprecated) which is now didn't use

--- src/structs/account_struct.rs
+++ src/structs/account_struct.rs
@@ -4,18 +4,6 @@ use serde::{Deserialize, Serialize};
 use sqlx::prelude::FromRow;
 use ts_rs::TS;
 
-// REQUEST STRUCTS
-#[derive(Deserialize, Debug, TS)]
-#[ts(export, export_to = "../../src/ServerTypes/Account.ts")]
-pub struct AccountRegistration {
-    pub firstname: String,
-    pub middlename: String,
-    pub lastname: String,
-    pub username: String,
-    pub password: String,
-    pub confirmpassword: String
-}
-
 #[derive(Deserialize, Debug, TS)]
 #[ts(export, export_to = "../../src/ServerTypes/Account.ts")]
 pub struct AccountLogin {
