80483a09229515c7757103945a9a53b2cde7412d
John Mark Banisilon <zionsploit@gmail.com>
2025-02-22 17:28:27 +0800

    refactor(Development): :fire: remove /registration endpoint
    /add-credentials endpoint well now be based for registering newly account, and have now different structs

--- src/routes/account_route.rs
+++ src/routes/account_route.rs
@@ -1,14 +1,9 @@
 use axum::{routing::post, Router};
 
-use crate::services::account_service::{add_account_credentials, login_account, register_account, verify_token};
+use crate::services::account_service::{add_account_credentials, login_account, verify_token};
 
 pub fn account_routes () -> Router {
     Router::new()
-        // Remove to the future /registration
-        // Since it'll used the /add-credentials
-        .route("/registration", post(register_account))
-        
-        
         .route("/login", post(login_account ))
         .route("/verify-token", post(verify_token ))
         // NEW API IMPLEMENTATIONS
