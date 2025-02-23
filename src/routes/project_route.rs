3376c9030c2f70f6ec4d7d634886b8755621d9f7
John Mark Banisilon <zionsploit@gmail.com>
2025-02-23 22:21:52 +0800

    feat(Development): :sparkles: Add new routes for get all project status

--- src/routes/project_route.rs
+++ src/routes/project_route.rs
@@ -1,6 +1,6 @@
 use axum::{routing::{get, post}, Router};
 
-use crate::services::project_service::{add_project, add_project_details, delete_project_by_id, get_all_projects, update_project_by_id};
+use crate::services::{project_service::{add_project, add_project_details, delete_project_by_id, get_all_projects, update_project_by_id}, project_status_service::get_all_project_status};
 
 pub fn project_route () -> Router {
     Router::new()
@@ -9,4 +9,6 @@ pub fn project_route () -> Router {
         .route("/update-by-id", post(update_project_by_id))
         .route("/delete-by-id", post(delete_project_by_id))
         .route("/get-all", get(get_all_projects))
+        // PROJECT STATUS
+        .route("/get-all-project-status", get(get_all_project_status))
 }
